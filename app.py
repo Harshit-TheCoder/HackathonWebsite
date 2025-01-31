7feb5e732901d8f39861652ecec71fc216b8d89a
Harshit-TheCoder <harshitharlalka11@gmail.com>
2025-01-31 22:01:00 +0530

    emotion model connected

--- app.py
+++ app.py
@@ -2,21 +2,39 @@ from flask import Flask, render_template, request
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
+import joblib
+import asyncio
 import pickle as pkl
+from googletrans import Translator
+
+# Initialize the translator
+translator = Translator()
+
 app = Flask(__name__)
 
 emotion_array = ['sadness', 'joy' , 'love', 'anger', 'fear', 'suppride']
 emotion_emoji = [ '😞 or 😢', '😊 or 😄', '❤️ or 😍', '😡 or 😠', '😨 or 😱', '😌 or 🦚']
 
 model = load_model('trained_weights/lstm_best_model.h5')
-with open('trained_weights/wordpiece_tokenizer.pkl', 'rb') as f:
-    tokenizer = pkl.load(f)
+tkn = pkl.load(open('trained_weights/wordpiece.pkl', 'rb'))
+v = joblib.load("trained_weights/tfv.pkl")
 
 @app.route('/predict', methods=['POST'])
-def predict():
+async def predict():
     # sample_text = "i would think that whomever would be lucky enough to stay in this suite must feel like it is the most romantic place on earth"
     sample_text = request.form.get('sentence')
-    output = tokenizer.encode(sample_text)
+    print(f"Type of sample_text: {type(sample_text)}, Value: {sample_text}")
+    language = request.form.get('language')
+    print(f"Detected language: {language}")
+    
+    # Translate the text if it is not already in English
+    if language != 'en':
+        translated_text =await translator.translate(sample_text, src=language, dest='en')
+    else:
+        translated_text = sample_text
+
+    print(f"Translated text: {translated_text}")
+    output = tkn.encode(sample_text)
     print("Tokens:", output.tokens)
     print("Token IDs:", output.ids)
     data = output.ids
